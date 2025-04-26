32473413b759bb43ab8ce0bf82a49bff30bbf12e
Bartosz Bryniarski <brylka@jelenia.com>
2025-04-26 12:55:28 +0200

    Sobota

--- application.py
+++ application.py
@@ -7,6 +7,16 @@ application = Flask(__name__)
 
 ENV = os.environ.get('FLASK_ENV', 'developerskie')
 
+# Konfiguracja zależna od środowiska
+if ENV == 'produkcja':
+    # Ustawienia dla wersji produkcyjnej
+    # np. połączenie z bazą danych
+    pass
+else:
+    # Ustawienia dla wersji developerskiej
+    # np. użycie bazy sqlite
+    pass
+
 @application.route('/')
 def hello_world():
     return(f'<h1>Hello, BigData z Pythonem!</h1><p>Przykład CI/CD - automatyczne wdrażanie!</p>'
